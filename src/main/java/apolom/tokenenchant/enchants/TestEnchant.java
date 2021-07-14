96673c1792a76c20314f19d76e6681742a6d5b3d
apolomachado <76997451+apolomachado@users.noreply.github.com>
2021-07-14 11:33:13 +0100

    Commit

--- src/main/java/apolom/tokenenchant/enchants/TestEnchant.java
+++ src/main/java/apolom/tokenenchant/enchants/TestEnchant.java
@@ -14,44 +14,51 @@ import org.bukkit.event.block.BlockBreakEvent;
 
 import java.util.HashMap;
 
-public class Test extends EnchantHandler {
+public class TestEnchant extends EnchantHandler {
 
-    private HashMap<Player, Integer> brokenBlocks = new HashMap<>();
+    private final HashMap<Player, Integer> brokenBlocks = new HashMap<>();
 
-    public HashMap<Player, Integer> getBrokenBlocks() {
+    private HashMap<Player, Integer> getBrokenBlocks() {
         return brokenBlocks;
     }
 
-    public Test(TokenEnchantAPI plugin) throws InvalidTokenEnchantException {
+    private String description;
+
+    public TestEnchant(TokenEnchantAPI plugin) throws InvalidTokenEnchantException {
         this(plugin, null, null);
     }
 
-    public Test(TokenEnchantAPI plugin, String name, FileConfiguration config) throws InvalidTokenEnchantException {
+    public TestEnchant(TokenEnchantAPI plugin, String name, FileConfiguration config) throws InvalidTokenEnchantException {
         super(plugin, name, config);
         loadConfig();
     }
 
     public void loadConfig() {
         super.loadConfig();
+        this.description = this.config.getString("Enchants.Test.Description", "Description.");
     }
 
     public String getName() {
         return "Test";
     }
 
-    @Override
+    public String getDescription() {
+        return description;
+    }
+
     public String getVersion() {
         return "1.0.0";
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(strings[0].equalsIgnoreCase("testing")) {
+        if (strings[0].equalsIgnoreCase("testing")) {
             commandSender.sendMessage("123");
         }
         return super.onCommand(commandSender, command, s, strings);
     }
 
+    @Deprecated
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent e) {
         Player p = e.getPlayer();
@@ -59,7 +66,7 @@ public class Test extends EnchantHandler {
         int level = getCELevel(p.getItemInHand());
 
         if (canExecute(level) && checkCooldown(p) && isValid(broken.getLocation())) {
-            if(getBrokenBlocks().containsKey(e.getPlayer()))
+            if (getBrokenBlocks().containsKey(e.getPlayer()))
                 getBrokenBlocks().put(
                         e.getPlayer(),
                         getBrokenBlocks().get(e.getPlayer()) + 1
